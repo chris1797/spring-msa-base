654fd203d0f59035833bfcfe76d0c2c8a3ca7d24
chris1797 <jaehun6004@gmail.com>
2025-05-25 13:08:03 +0900

    feat: order entity

--- order-service/order/src/main/java/com/msa/order/domain/Order.java
+++ order-service/order/src/main/java/com/msa/order/domain/Order.java
@@ -1,18 +1,36 @@
 package com.msa.order.domain;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
 @Entity
 @Table(name = "orders")
+@EntityListeners(AuditingEntityListener.class)
 public class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
     @Column(name = "product_name", nullable = false)
     private String productName;
 
+    @Column(name = "quantity", nullable = false)
+    private int quantity;
 
+    @Column(name = "price", nullable = false)
+    private BigDecimal price;
 
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
 }
